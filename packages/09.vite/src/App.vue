93af18bb7627118975916d14e7bf3689cf9e37d5
wangkaiwd <1162486176@qq.com>
2022-02-03 13:57:06 +0800

    feat: create vite demo

---
+++ packages/09.vite/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">
+    app
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup () {
+    const state = reactive({});
+    return {
+      ...toRefs(state)
+    };
+  },
+});
+</script>
+
+<style lang="less" scoped>
+.app {
+
+}
+</style>
