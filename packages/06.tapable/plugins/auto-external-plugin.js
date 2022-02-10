b0ff4a38d431bb6d928a280eb26189d00f7ec823
wangkaiwd <1162486176@qq.com>
2022-02-10 18:18:18 +0800

    feat: refactor plugin code

--- packages/06.tapable/plugins/auto-external-plugin.js
+++ packages/06.tapable/plugins/auto-external-plugin.js
@@ -12,34 +12,46 @@ class AutoExternalPlugin {
 
   apply (compiler) {
     compiler.hooks.normalModuleFactory.tap(this.name, (normalModuleFactory) => {
-      normalModuleFactory.hooks.parser.for('javascript/auto').tap(this.name, (parser) => {
-        // imported module
-        parser.hooks.import.tap(this.name, (statement, source) => {
-          if (this.externalModules.includes(source)) {
-            this.importedModules.add(source);
-          }
-        });
-        // required module
-        parser.hooks.call.for('require').tap(this.name, (expression) => {
-          const source = expression.arguments[0].value;
-          if (this.externalModules.includes(source)) {
-            this.importedModules.add(source);
-          }
-        });
+      this.getImportedPackage(normalModuleFactory);
+      this.createGlobalVariable(normalModuleFactory);
+    });
+    this.injectScriptsToHTML(compiler);
+  }
+
+  getImportedPackage (normalModuleFactory) {
+    normalModuleFactory.hooks.parser.for('javascript/auto').tap(this.name, (parser) => {
+      // imported module
+      parser.hooks.import.tap(this.name, (statement, source) => {
+        if (this.externalModules.includes(source)) {
+          this.importedModules.add(source);
+        }
       });
-      // AsyncSeriesBailHook
-      normalModuleFactory.hooks.factorize.tapAsync(this.name, (resolveData, cb) => {
-        const { request } = resolveData;
-        if (this.externalModules.includes(request)) {
-          const { variable } = this.options[request];
-          // return non-undefined will stop execute subsequent hook
-          // ExternalModule do what ?
-          cb(null, new ExternalModule(variable, 'window', request));
-        } else {
-          cb(null);
+      // required module
+      parser.hooks.call.for('require').tap(this.name, (expression) => {
+        const source = expression.arguments[0].value;
+        if (this.externalModules.includes(source)) {
+          this.importedModules.add(source);
         }
       });
     });
+  }
+
+  createGlobalVariable (normalModuleFactory) {
+    // AsyncSeriesBailHook
+    normalModuleFactory.hooks.factorize.tapAsync(this.name, (resolveData, cb) => {
+      const { request } = resolveData;
+      if (this.externalModules.includes(request)) {
+        const { variable } = this.options[request];
+        // return non-undefined will stop execute subsequent hook
+        // ExternalModule do what ?
+        cb(null, new ExternalModule(variable, 'window', request));
+      } else {
+        cb(null);
+      }
+    });
+  }
+
+  injectScriptsToHTML (compiler) {
     compiler.hooks.compilation.tap(this.name, (compilation) => {
       HtmlWebpackPlugin.getHooks(compilation).alterAssetTags.tapAsync(this.name, (data, cb) => {
         this.importedModules.forEach((packageName) => {
